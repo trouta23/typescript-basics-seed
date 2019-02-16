832d2926e7dcc99c8c7c4ecd6277bc445884accd
Austin Trout <trouta23@gmail.com>
2019-02-16 11:30:56 -0500

    Arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log('Hello TypeScript!');
+const pizzas = [
+  { name: 'Pepperoni', toppings: ['pepperoni'] },
+  { name: 'Cheese', toppings: ['cheese'] },
+];
+
+// OG way
+const mappedPizzas = pizzas.map(function(pizza) {
+  return pizza.name.toUpperCase();
+});
+
+// Arrow function
+const mappedPizzas2 = pizzas.map(pizza => {
+  return pizza.name.toUpperCase();
+});
+
+// We can also pass other params like `index`
+// const mappedPizzas2 = pizzas.map((pizza, index) => {
+//   return pizza.name.toUpperCase();
+//   // return index;
+// });
+
+// Implicit return arrow function
+// Need to research arrow functions with `this`
+// If it's just a one liner, we can get away with doing something like this:
+const mappedPizzas3 = pizzas.map(pizza => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+console.log(mappedPizzas2);
+console.log(mappedPizzas3);
