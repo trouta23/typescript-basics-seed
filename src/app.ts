6a4abcd84f7bbce1cc637720d5ab680934a3f6d9
Austin Trout <trouta23@gmail.com>
2019-02-16 11:55:11 -0500

    Default parameters

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,10 @@
-const pizzas = [
-  { name: 'Pepperoni', toppings: ['pepperoni'] },
-  { name: 'Cheese', toppings: ['cheese'] },
-];
+function multiply(x: number, y: number = 25) {
+  // Effectively saying:
+  // if (!b) {
+  //   y = 25
+  // }
+  return x * y;
+}
 
-// OG way
-const mappedPizzas = pizzas.map(function(pizza) {
-  return pizza.name.toUpperCase();
-});
-
-// Arrow function
-const mappedPizzas2 = pizzas.map(pizza => {
-  return pizza.name.toUpperCase();
-});
-
-// We can also pass other params like `index`
-// const mappedPizzas2 = pizzas.map((pizza, index) => {
-//   return pizza.name.toUpperCase();
-//   // return index;
-// });
-
-// Implicit return arrow function
-// Need to research arrow functions with `this`
-// If it's just a one liner, we can get away with doing something like this:
-const mappedPizzas3 = pizzas.map(pizza => pizza.name.toUpperCase());
-
-console.log(mappedPizzas);
-console.log(mappedPizzas2);
-console.log(mappedPizzas3);
+console.log(multiply(5));
+console.log(multiply(2, 8));
