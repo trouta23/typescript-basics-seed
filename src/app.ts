c0b494180b599168e1a22b62e8a1bf61b23da4f0
Austin Trout <trouta23@gmail.com>
2019-02-16 12:12:34 -0500

    Object literal improvements

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,42 @@
-function multiply(x: number, y: number = 25) {
-  // Effectively saying:
-  // if (!b) {
-  //   y = 25
-  // }
-  return x * y;
+// OG Way
+const pizza = {
+  name: 'Pepperoni',
+  price: 15,
+  getName: function() {
+    return this.name;
+  },
+};
+
+// With ES6
+const pizza2 = {
+  name: 'Pepperoni',
+  price: 15,
+  getName() {
+    return this.name;
+  },
+};
+
+console.log(pizza.getName());
+console.log(pizza.getName());
+
+const toppings = ['pepperoni'];
+
+// OG way
+const order = {
+  pizza: pizza,
+  toppings: toppings,
+};
+
+// With ES5 or ES6 we are able to do:
+const order2 = {
+  pizza,
+  toppings,
+};
+
+function createOrder(pizza: object, toppings: object) {
+  return { pizza, toppings };
 }
 
-console.log(multiply(5));
-console.log(multiply(2, 8));
+console.log(order);
+console.log(order2);
+console.log(createOrder(pizza, toppings));
